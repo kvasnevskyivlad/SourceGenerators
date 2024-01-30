f1665fa5c1cacb41a6b3f5348ee3515c4e58f58a
Vlad Kvasnevskyi <vladyslav.kvasnevskyi@materialise.kiev.ua>
2024-01-30 15:34:55 +0200

    Started using Semantic model instead of Symbol one. Now it is possible to collect properties even with partial classes (e.g. you can make partial Person class with one more property and you should not mark it with an attriburte, semantic model anyway is able to read all members including members from partial classes).

---
+++ Generators/Model/ClassToGenerate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Generators.Model;
+
+internal class ClassToGenerate
+{
+    public ClassToGenerate(
+        string namespaceName,
+        string className,
+        IEnumerable<string> propertyNames)
+    {
+        NamespaceName = namespaceName;
+        ClassName = className;
+        PropertyNames = propertyNames;
+    }
+
+    public string NamespaceName { get; }
+    public string ClassName { get; }
+    public IEnumerable<string> PropertyNames { get; }
+}
--- Generators/ToStringGenerator.cs
+++ Generators/ToStringGenerator.cs
@@ -1,75 +1,93 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
+using Generators.Model;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace Generators
+namespace Generators;
+
+[Generator]
+public class ToStringGenerator : IIncrementalGenerator
 {
-    [Generator]
-    public class ToStringGenerator : IIncrementalGenerator
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        public void Initialize(IncrementalGeneratorInitializationContext context)
-        {
-            // Create and configure syntax provider to get all classes.
-            var classes = context.SyntaxProvider.CreateSyntaxProvider(
+        // Create and configure syntax provider to get all classes.
+        var classes = context.SyntaxProvider.CreateSyntaxProvider(
                 static (node, _) => IsSyntaxTarget(node),
                 static (ctx, _) => GetSemanticTarget(ctx))
-                .Where(static target => target is not null);
+            .Where(static target => target is not null);
 
-            // Register output generation for each class.
-            context.RegisterSourceOutput(classes,
-                static (ctx, source) => Execute(ctx, source!));
+        // Register output generation for each class.
+        context.RegisterSourceOutput(classes,
+            static (ctx, source) => Execute(ctx, source));
 
-            // Add attribute implementation.
-            context.RegisterPostInitializationOutput(PostInitializationOutput);
-        }
+        // Add attribute implementation.
+        context.RegisterPostInitializationOutput(PostInitializationOutput);
+    }
 
-        private static bool IsSyntaxTarget(SyntaxNode node)
+    private static bool IsSyntaxTarget(SyntaxNode node)
+    {
+        return node is ClassDeclarationSyntax classDeclarationSyntax && classDeclarationSyntax.AttributeLists.Count > 0;
+    }
+
+    private static ClassToGenerate? GetSemanticTarget(GeneratorSyntaxContext context)
+    {
+        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+        var classSymbol = context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax);
+        var attributeSymbol =
+            context.SemanticModel.Compilation.GetTypeByMetadataName(
+                "Generators.ToStringGenerator.GenerateToStringAttribute");
+
+        if (classSymbol is not null && attributeSymbol is not null)
         {
-            return node is ClassDeclarationSyntax classDeclarationSyntax && classDeclarationSyntax.AttributeLists.Count > 0;
-        }
-
-        private static ClassDeclarationSyntax? GetSemanticTarget(GeneratorSyntaxContext context)
-        {
-            var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
-            var classSymbol = context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax);
-            var attributeSymbol =
-                context.SemanticModel.Compilation.GetTypeByMetadataName(
-                    "Generators.ToStringGenerator.GenerateToStringAttribute");
-
-            if (classSymbol is not null && attributeSymbol is not null)
+            foreach (var attributeData in classSymbol.GetAttributes())
             {
-                foreach (var attributeData in classSymbol.GetAttributes())
+                if (attributeSymbol.Equals(attributeData.AttributeClass, SymbolEqualityComparer.Default))
                 {
-                    if (attributeSymbol.Equals(attributeData.AttributeClass, SymbolEqualityComparer.Default))
+                    var namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
+                    var className = classSymbol.Name;
+                    var propertyNames = new List<string>();
+
+                    foreach (var memberSymbol in classSymbol.GetMembers())
                     {
-                        return classDeclarationSyntax;
+                        if (memberSymbol.Kind == SymbolKind.Property &&
+                            memberSymbol.DeclaredAccessibility == Accessibility.Public)
+                        {
+                            propertyNames.Add(memberSymbol.Name);
+                        }
                     }
+
+                    return new ClassToGenerate(namespaceName, className, propertyNames);
                 }
             }
-
-            return null;
         }
 
-        private static void PostInitializationOutput(IncrementalGeneratorPostInitializationContext context)
-        {
-            context.AddSource("Generators.ToStringGenerator.GenerateToStringAttribute.g.cs",
-                @"namespace Generators.ToStringGenerator
+        return null;
+    }
+
+    private static void PostInitializationOutput(IncrementalGeneratorPostInitializationContext context)
+    {
+        context.AddSource("Generators.ToStringGenerator.GenerateToStringAttribute.g.cs",
+            @"namespace Generators.ToStringGenerator
 {
     internal class GenerateToStringAttribute : System.Attribute {}
 }");
+    }
+
+    private static void Execute(SourceProductionContext context, ClassToGenerate? classToGenerate)
+    {
+        if (classToGenerate is null)
+        {
+            return;
         }
 
-        private static void Execute(SourceProductionContext context, ClassDeclarationSyntax classDeclarationSyntax)
-        {
-            if (classDeclarationSyntax.Parent is BaseNamespaceDeclarationSyntax namespaceDeclarationSyntax)
-            {
-                var namespaceName = namespaceDeclarationSyntax.Name.ToString();
-                var className = classDeclarationSyntax.Identifier.Text;
-                var fileName = $"{namespaceName}.{className}.g.cs";
+        var namespaceName = classToGenerate.NamespaceName;
+        var className = classToGenerate.ClassName;
+        var fileName = $"{namespaceName}.{className}.g.cs";
 
-                var stringBuilder = new StringBuilder();
-                stringBuilder.Append($@"namespace {namespaceName}
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append($@"namespace {namespaceName}
 {{
     partial class {className}
     {{
@@ -77,26 +95,27 @@ namespace Generators
         {{
             return $""");
 
-            for (var i = 0; i < classDeclarationSyntax.Members.Count; i++)
+        var first = true;
+        foreach (var propertyName in classToGenerate.PropertyNames)
+        {
+            if (first)
             {
-                if (classDeclarationSyntax.Members[i] is PropertyDeclarationSyntax propertyDeclarationSyntax &&
-                    propertyDeclarationSyntax.Modifiers.Any(SyntaxKind.PublicKeyword))
-                {
-                    if (i > 0) stringBuilder.Append("; ");
-
-                    var propertyName = propertyDeclarationSyntax.Identifier.Text;
-                    stringBuilder.Append($"{propertyName}:{{{propertyName}}}");
-                }
+                first = false;
+            }
+            else
+            {
+                stringBuilder.Append("; ");
             }
 
-            stringBuilder.Append($@""";
-        }}
-    }}
-}}
+            stringBuilder.Append($"{propertyName}:{{{propertyName}}}");
+        }
+
+        stringBuilder.Append(@""";
+        }
+    }
+}
 ");
 
-            context.AddSource(fileName, stringBuilder.ToString());
-            }
-        }
+        context.AddSource(fileName, stringBuilder.ToString());
     }
 }
