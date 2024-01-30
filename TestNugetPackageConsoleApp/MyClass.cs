2a229a433d0b2216072e182a78be5103102cc847
Vlad Kvasnevskyi <vladyslav.kvasnevskyi@materialise.kiev.ua>
2024-01-30 16:32:56 +0200

    Used nuget package in test project.

---
+++ TestNugetPackageConsoleApp/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using Generators.ToStringGenerator;
+
+namespace TestNugetPackageConsoleApp
+{
+    [GenerateToString]
+    public partial class MyClass
+    {
+        public string? Test1 { get; set; }
+        public int Test2 { get; set; }
+    }
+}
---
+++ TestNugetPackageConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using TestNugetPackageConsoleApp;
+
+var test = new MyClass
+{
+    Test1 = "Test1",
+    Test2 = 10
+};
+
+Console.WriteLine(test);
+Console.ReadLine();
