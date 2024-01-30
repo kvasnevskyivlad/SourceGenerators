cc81128d882cf096a2387791edb1e04bc9e1e1ca
Vlad Kvasnevskyi <vladyslav.kvasnevskyi@materialise.kiev.ua>
2024-01-30 15:54:29 +0200

    Added Equal to ClassToGenerate in order to trigger using of internal source generator cache, so source generator will generate file only when there is a change in corresponing class and that change is relevant.

--- Generators/Model/ClassToGenerate.cs
+++ Generators/Model/ClassToGenerate.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Generators.Model;
 
-internal class ClassToGenerate
+internal class ClassToGenerate : IEquatable<ClassToGenerate?>
 {
     public ClassToGenerate(
         string namespaceName,
@@ -17,4 +19,25 @@ internal class ClassToGenerate
     public string NamespaceName { get; }
     public string ClassName { get; }
     public IEnumerable<string> PropertyNames { get; }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as ClassToGenerate);
+    }
+
+    public bool Equals(ClassToGenerate? other)
+    {
+        return other is not null &&
+               NamespaceName == other.NamespaceName &&
+               ClassName == other.ClassName &&
+               PropertyNames.SequenceEqual(other.PropertyNames);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (NamespaceName.GetHashCode() * 397) ^ ClassName.GetHashCode();
+        }
+    }
 }
